5d64136728d242edeba941ecc759724fbb1966aa
usamalodhi <43435750+usamalodhi@users.noreply.github.com>
2022-06-26 19:18:32 +0100

    Add a custom hook to detect when custom fonts have finished loading

--- src/utils/useIsFontReady.tsx
+++ src/utils/useIsFontReady.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-// a custom hook to detect when custom fonts have finished loading
 export function useIsFontReady() {
   const [isReady, setIsReady] = React.useState(false);
 
