9c920747e044cdf1513d1583d3ce11881e9c3821
usamalodhi <43435750+usamalodhi@users.noreply.github.com>
2022-06-26 19:17:49 +0100

    Add constants tsx file

---
+++ src/utils/constants.tsx
@@ -0,0 +1,3 @@
+export const GITHUB_PROFILE = 'https://www.github.com/usamalodhi';
+export const WEBSITE = 'https://www.google.com';
+export const LIGHT_COLORS = ['#A7F3D0', '#BAE6FD', '#C7D2FE', '#FDE68A'];
