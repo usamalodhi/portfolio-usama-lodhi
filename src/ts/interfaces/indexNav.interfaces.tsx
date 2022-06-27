436a28184005d2a7f2bbfe6ee3130aa23b721b40
usamalodhi <43435750+usamalodhi@users.noreply.github.com>
2022-06-27 14:56:23 +0100

    Add interfaces for nav on indexpage and social icons

---
+++ src/ts/interfaces/indexNav.interfaces.tsx
@@ -0,0 +1,7 @@
+interface indexNavInterface {
+  id: number;
+  href: string;
+  title: string;
+}
+
+export default indexNavInterface;
---
+++ src/ts/interfaces/social.interfaces.tsx
@@ -0,0 +1,9 @@
+interface socialInterface {
+  id: number;
+  image: string;
+  alt: string;
+  title: string;
+  linkTo: string;
+}
+
+export default socialInterface;
