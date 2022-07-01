1926ac7d3a4e087523c1518169381b426ed29c2f
usamalodhi <43435750+usamalodhi@users.noreply.github.com>
2022-07-01 09:34:54 +0100

    Update Footer with social links and copywrite text

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,19 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Flex } from '@chakra-ui/react';
+import NextLink from 'next/link';
 
-export function Footer() {
-  return <Box as='footer'>Footer goes here</Box>;
-}
+import SocialLinks from '../SocialLinks/SocialLinks';
+
+const Footer = () => {
+  return (
+    <Flex as='footer' justifyContent='space-between' alignItems='center' padding='16px 24px'>
+      <Box>
+        <NextLink href='/'>© 2022 Usama Lodhi</NextLink>
+      </Box>
+      <Box>
+        <SocialLinks />
+      </Box>
+    </Flex>
+  );
+};
+
+export default Footer;
