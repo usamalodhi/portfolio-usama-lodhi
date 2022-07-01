82f77c3cf41375006ea59a191ebb9327b3fef2d0
usamalodhi <43435750+usamalodhi@users.noreply.github.com>
2022-07-01 09:37:02 +0100

    Update Projects section with react slick carousel -WIP

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,10 +1,120 @@
-import { Box, Text } from '@chakra-ui/react';
+import React from 'react';
+import { Box, IconButton, useBreakpointValue, Stack, Heading, Text, Container } from '@chakra-ui/react';
+// Here we have used react-icons package for the icons
+import { BiLeftArrowAlt, BiRightArrowAlt } from 'react-icons/bi';
+// And react-slick as our Carousel Lib
+import Slider from 'react-slick';
+import projectImg from '../../../public/assets/featuredProject.jpg';
 
-export function Projects() {
+// Settings for the slider
+const settings = {
+  dots: true,
+  arrows: false,
+  fade: true,
+  infinite: true,
+  autoplay: true,
+  speed: 500,
+  autoplaySpeed: 5000,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+};
+
+const Projects = () => {
+  // As we have used custom buttons, we need a reference variable to
+  // change the state
+  const [slider, setSlider] = React.useState<Slider | null>(null);
+
+  // These are the breakpoints which changes the position of the
+  // buttons as the screen size changes
+  const top = useBreakpointValue({ base: '90%', md: '50%' });
+  const side = useBreakpointValue({ base: '30%', md: '40px' });
+
+  // This list contains all the data for carousels
+  // This can be static or loaded from a server
+  const cards = [
+    {
+      title: 'Design Projects 1',
+      text: "The project board is an exclusive resource for contract work. It's perfect for freelancers, agencies, and moonlighters.",
+      image: { projectImg },
+    },
+    {
+      title: 'Design Projects 2',
+      text: "The project board is an exclusive resource for contract work. It's perfect for freelancers, agencies, and moonlighters.",
+      image: { projectImg },
+    },
+  ];
+
   return (
-    <Box>
-      <Text as='h1'>Project Section </Text>
-      <Text as='p'>Write Text Here For The Project Section </Text>
-    </Box>
+    <>
+      <Box layerStyle='scrollToBox' id='projects'>
+        <Text as='h2'>Projects </Text>
+      </Box>
+      <Box position={'relative'} height={'600px'} width={'full'} overflow={'hidden'}>
+        {/* CSS files for react-slick */}
+        <link
+          rel='stylesheet'
+          type='text/css'
+          charSet='UTF-8'
+          href='https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css'
+        />
+        <link
+          rel='stylesheet'
+          type='text/css'
+          href='https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css'
+        />
+        {/* Left Icon */}
+        <IconButton
+          aria-label='left-arrow'
+          variant='ghost'
+          position='absolute'
+          left={side}
+          top={top}
+          transform={'translate(0%, -50%)'}
+          zIndex={2}
+          onClick={() => slider?.slickPrev()}
+        >
+          <BiLeftArrowAlt size='40px' />
+        </IconButton>
+        {/* Right Icon */}
+        <IconButton
+          aria-label='right-arrow'
+          variant='ghost'
+          position='absolute'
+          right={side}
+          top={top}
+          transform={'translate(0%, -50%)'}
+          zIndex={2}
+          onClick={() => slider?.slickNext()}
+        >
+          <BiRightArrowAlt size='40px' />
+        </IconButton>
+        {/* Slider */}
+        <Slider {...settings} ref={(slider) => setSlider(slider)}>
+          {cards.map((card, index) => (
+            <Box
+              key={index}
+              height={'6xl'}
+              position='relative'
+              backgroundPosition='center'
+              backgroundRepeat='no-repeat'
+              backgroundSize='cover'
+              backgroundImage={`url(${card.image})`}
+            >
+              {/* This is the block you need to change, to customize the caption */}
+              <Container size='container.lg' height='600px' position='relative'>
+                <Stack spacing={6} w={'full'} maxW={'lg'} position='absolute' top='50%' transform='translate(0, -50%)'>
+                  <Heading fontSize={{ base: '3xl', md: '4xl', lg: '5xl' }}>{card.title}</Heading>
+                  <Text fontSize={{ base: 'md', lg: 'lg' }} color='GrayText'>
+                    {card.text}
+                  </Text>
+                </Stack>
+              </Container>
+            </Box>
+          ))}
+        </Slider>
+      </Box>
+    </>
   );
-}
+};
+
+export default Projects;
