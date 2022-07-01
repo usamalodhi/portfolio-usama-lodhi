d05c84bee2147695743b8b82b905ff7c9e4995a4
usamalodhi <43435750+usamalodhi@users.noreply.github.com>
2022-07-01 08:28:16 +0100

    Update Header with logo and nav links

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,28 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Flex, List, ListItem, useStyleConfig } from '@chakra-ui/react';
+import NextLink from 'next/link';
+import indexNavData from '../../ts/data/indexNav.data';
 
-export function Header() {
-  return <Box as='header'>Header goes here</Box>;
-}
+const Header = () => {
+  const styles = useStyleConfig('Header');
+
+  return (
+    <Flex __css={styles} flexDirection='row'>
+      <Box textStyle='Logo'>
+        <NextLink href='/'>USAMA</NextLink>
+      </Box>
+      <Box>
+        <List display='flex' gap='24px'>
+          {indexNavData.map((data) => {
+            return (
+              <ListItem key={data.id}>
+                <NextLink href={data.href}>{data.title}</NextLink>
+              </ListItem>
+            );
+          })}
+        </List>
+      </Box>
+    </Flex>
+  );
+};
+
+export default Header;
