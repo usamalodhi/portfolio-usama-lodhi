e2d48dafb990468f5ab8a56ae70f3b34f452e60e
usamalodhi <43435750+usamalodhi@users.noreply.github.com>
2022-07-01 09:00:05 +0100

    Update Hero with heading, subheading, message and CTA buttons to review LinkedIn and Github

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,10 +1,32 @@
-import { Box, Text } from '@chakra-ui/react';
+import { Text, Button, Flex, Image, Stack } from '@chakra-ui/react';
 
-export function Hero() {
+// Hello, I'm
+// Usama Lodhi
+// Providing Solutions To Your Problems
+// Front End Developer with a background in finance and research. Currently searching for new and exciting opportunities.
+
+const Hero = () => {
   return (
-    <Box>
-      <Text as='h1'>Usama Lodhi Developer Portfolio </Text>
-      <Text as='p'>Write Text Here For The Hero Section </Text>
-    </Box>
+    <Stack minH='100vh' direction={{ base: 'column', md: 'row' }}>
+      <Flex flex={1} align={'center'} justify={'center'}>
+        <Stack>
+          <Text as='h2'>
+            Hello, I'm <br />
+            Usama Lodhi
+          </Text>
+          <Text as='h1'>Providing Solutions To Your Problems</Text>
+          <Text as='h2'>
+            Front End Developer with a background in finance and research. Currently searching for new and exciting
+            opportunities
+          </Text>
+          <Stack padding='16px 24px' direction='row'>
+            <Button m='0px'>LinkedIn</Button>
+            <Button m='0px'>Github</Button>
+          </Stack>
+        </Stack>
+      </Flex>
+    </Stack>
   );
-}
+};
+
+export default Hero;
